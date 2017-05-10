a474b081730b5d5af28e47b53640ff945dab671f
nouraali3 <noura.ali9@yahoo.com>
2017-05-10 16:14:43 +0300

    first,best,worst fit allocation

---
+++ memory mapping/src/memory/mapping/MemoryMapping.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memory.mapping;
+
+/**
+ *
+ * @author user
+ */
+public class MemoryMapping {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        holetable obj=new holetable();
+        obj.setVisible(true);
+    }
+    
+}
